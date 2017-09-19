12e157cf0400db92b7007bf24c4c34679a7cb997
Tarun <tarun.batra00@gmail.com>
2017-09-20 00:30:09 +0530

    Limits the search within specified site

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const isUrl = require('is-url');
 const events = require('events');
 const scraper = new events.EventEmitter();
 
-const SITE = process.env.SITE;
+const SITE = removeTrailingSlash(process.env.SITE);
 const CONCURRENCY_LIMIT = process.env.CONCURRENCY_LIMIT || 5;
 let concurrency = 0;
 let links = [];
 let scrapedLinks = [];
+
+/**
+ *  Remove trailing slashes in a string
+ *  @param {string} str - Input string
+ *  @returns {string} String with trailing slash trimmed
+ */
+function removeTrailingSlash(str = '') {
+  return str.replace(/\/$/, '');
+}
+
+/**
+ *  Checks if the link is in provided domain
+ *  @param {string} link - Link to check
+ *  @param {string} link - Link to check
+ *  @returns {boolean} True if link belongs to the domain
+ */
+function isInDoman(link = '', domain = '') {
+  return link.startsWith(domain);
+}
 
 /**
  *  Extract links from HTML
@@ -18,8 +37,13 @@ let scrapedLinks = [];
 function extractLinks(html) {
   const $ = cheerio.load(html);
   return $('a')
-           .toArray()
-           .map(tag => tag.attribs.href);
+          .toArray()
+          .map(tag => {
+            const href = removeTrailingSlash(tag.attribs.href);
+            return isUrl(href)
+              ? href
+              : `${SITE}${href.startsWith('/') ? '' : '/'}${href}`
+           });
 }
 
 /**
@@ -34,6 +58,7 @@ function sanitizeLinks(list) {
              && links.indexOf(link) === -1        // The link is not already in queue
              && list.indexOf(link) === index      // The link isn't in the current batch
              && scrapedLinks.indexOf(link) === -1 // The link isn't already screaped
+             && isInDoman(link, SITE)             // The link is in same domain as the SITE
            ));
 }
 
@@ -42,17 +67,21 @@ function sanitizeLinks(list) {
  *  @param {string} url - URL to scrape
  */
 function scrape(url) {
-  scrapedLinks.push(url);
-  request(url, (err, res, body) => {
-    if (err) return;
-    links = links.concat(sanitizeLinks(extractLinks(body)));
-    scraper.emit('finished', url);
+  return new Promise((resolve, reject) => {
+    scrapedLinks.push(url);
+    request(url, (err, res, body) => {
+      if (err) return reject(err);
+      links = links.concat(sanitizeLinks(extractLinks(body)));
+      scraper.emit('finished', url);
+      resolve(url);
+    });
   });
 }
 
 function throttleScraping(url, limit) {
   console.log(`Started scraping ${url}`);
-  scrape(url);
+  scrape(url)
+    .catch(console.error);
   concurrency++;
   scraper.on('finished', (link) => {
     console.log(`Concurrency: ${concurrency} | Finished: ${link}`);
@@ -60,10 +89,10 @@ function throttleScraping(url, limit) {
     concurrency--;
     if (!links.length) {
       console.log(scrapedLinks.join(','));
-      return;
     }
     while(concurrency < limit && links.length) {
-      scrape(links.shift());
+      scrape(links.shift())
+        .catch(console.error);
       concurrency++;
     }
   });
